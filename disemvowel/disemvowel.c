c582ee85928845cf6642a602551fc52e7f905e14
Kyle M Foss <fossx229@neoprene.morris.umn.edu>
2017-09-28 11:27:01 -0500

    --Added documentation

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -10,6 +10,7 @@ char* disemvowel(char* str) {
   int count = 0;
   int z = 0;
   char* result;
+  //loops over the array  to find non-vowels and keeps track of the amount
   for (i = 0; i < len; ++i) {
     if(str[i] == 'a' || str[i] == 'e' || str[i]=='i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U'){
     
@@ -17,7 +18,9 @@ char* disemvowel(char* str) {
        count++;
     }
   }
+  //grabs memory for the result based on the amount of vowels
   result = (char*) calloc(count+1, sizeof(char));
+  //copies over non-vowels from the old array to the new one.
   for (i = 0; i < len; ++i) {
     if(str[i] == 'a' || str[i] == 'e' || str[i]=='i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U'){
     
@@ -27,7 +30,7 @@ char* disemvowel(char* str) {
     }
   }
 
-
+//Adding the null terminator at the end
   result[count] = '\0';
   return result;
 }
